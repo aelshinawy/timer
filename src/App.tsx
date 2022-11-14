192a913cbbea75430c864399ca0ba926e7eccc44
Ahmed El-Shinawy <aelshinawy92@gmail.com>
2022-11-15 01:04:01 +0200

    Initialize react app
    - Installed react router
    - Installed tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+   <div className="flex flex-col bg-slate-300 h-full items-center justify-center text-4xl">Timer</div>
+  );
+}
+
+export default App;
